e801de1618890d313db4713f65939344e1c77047
BenjiSomur <juan.somur@gmail.com>
2019-09-30 11:55:49 -0500

    Agrego hoja de estilos para almacén y llamadas a procedimiento almacenado para cerrar mes

--- resources/views/almacen/index.blade.php
+++ resources/views/almacen/index.blade.php
@@ -1,6 +1,10 @@
 @extends('adminlte::layouts.landing')
 
 @section('content')
+<link href="{{ asset('css/almacen.css') }}" rel="stylesheet" type="text/css" >
+<script src="https://kit.fontawesome.com/002f2479d1.js" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script type="text/javascript" src="{{ asset('js/almacen.js') }}"></script>
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -13,4 +17,5 @@
         </div>
     </div>
 </div>
+
 @endsection

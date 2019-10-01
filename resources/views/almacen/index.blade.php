b7687e58f84d30574b98536b22f3f98e3018679c
BenjiSomur <juan.somur@gmail.com>
2019-10-01 12:24:52 -0500

    Agrego vista de menu principal de administrador de almacén

--- resources/views/almacen/index.blade.php
+++ resources/views/almacen/index.blade.php
@@ -11,43 +11,43 @@
 		    <div class="row">
 	            @if(Auth::user()->name == 'almacen_admin')
 		           	<div class="col-xs-2">
-					<a href="{!! route('almacen.index') !!}">
-						<i class="fas fa-inbox"></i>
-						<span>Vales</span>
-					</a>
+						<a class="nombre_modulo" href="{!! route('almacen.index') !!}">
+							<i class="fas fa-inbox"></i>
+							<span>Vales</span>
+						</a>
 		           </div>
 		           <div class="col-xs-2">
-					<a href="{!! route('almacen.index') !!}">
+					<a class="nombre_modulo" href="{!! route('almacen.index') !!}">
 						<i class="fas fa-file-alt"></i>
 						<span>Reportes</span>
 					</a>
 		           </div>
 		           <div class="col-xs-2">
-					<a href="{!! route('almacen.index') !!}">
+					<a class="nombre_modulo" href="{!! route('almacen.index') !!}">
 						<i class="fas fa-book"></i>
 						<span>Polizas</span>
 					</a>
 		           </div>
 		           <div class="col-xs-2">
-					<a href="{!! route('almacen.index') !!}">
+					<a class="nombre_modulo" href="{!! route('almacen.index') !!}">
 						<i class="fas fa-lock"></i>
 						<span>Cerrar mes</span>
 					</a>
 		           </div>
 		           <div class="col-xs-2">
-					<a href="{!! route('almacen.index') !!}">
+					<a class="nombre_modulo" href="{!! route('almacen.index') !!}">
 						<i class="fas fa-boxes"></i>
 						<span>Artículos</span>
 					</a>
 		           </div>
 		           <div class="col-xs-2">
-					<a href="{!! route('almacen.index') !!}">
+					<a class="nombre_modulo" href="{!! route('almacen.index') !!}">
 						<i class="fas fa-layer-group"></i>
 						<span>Partidas</span>
 					</a>
 		           </div>
 		           <div class="col-xs-2">
-					<a href="{!! route('almacen.index') !!}">
+					<a class="nombre_modulo" href="{!! route('almacen.index') !!}">
 						<i class="fas fa-clipboard"></i>
 						<span>Facturas</span>
 					</a>

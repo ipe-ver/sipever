cc850182132ee23aa0d21ec2a4d5937c2e7c0a27
BenjiSomur <juan.somur@gmail.com>
2019-10-09 14:55:24 -0500

    Codificacion interfaz reportes

--- resources/views/almacen/reportes.blade.php
+++ resources/views/almacen/reportes.blade.php
@@ -95,19 +95,36 @@
                 </div>
             </div>
             <div class ="col-lg-6">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
-                    <label class="form-check-label" for="defaultCheck1">
-                    Botnon
-                    </label>
+                <div id="cosoPrueba">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="checkPorMes">
+                        <label class="form-check-label" for="checkPorMes">
+                        Generar reporte de un solo mes
+                        </label>
+                    </div>
                 </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
-                    <label class="form-check-label" for="defaultCheck1">
-                    Botnon
-                    </label>
+                <div class="row">
+                    <label class="text-nowrap col-xs-4">No. Mes</label>
+                    <div class="input-group spinner col-xs-3">
+                        <input name="numMes" id="no_mes" type="text" class="form-control" value="1" required>
+                        <div class="input-group-btn-vertical">
+                            <button id="mesIncrement" class="btn btn-default" type="button">
+                                <i class="fa fa-caret-up"></i>
+                            </button>
+                            <button id="mesDecrement" class="btn btn-default" type="button">
+                                <i class="fa fa-caret-down"></i>
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <button>Coso</button>
+                <label class="text-nowrap" style="padding-right: 43.5px;">Año</label>
+                <select name="year"class="spinner" style="text-align-last: right;" required>
+                    <option value="" dir="ltr">Año...</option>
+                    <option value="2019" dir="ltr">2019</option>
+                </select>
+                <div class="row">
+                    <button>Coso</button>
+                </div>
             </div>
         </div>
     </form>

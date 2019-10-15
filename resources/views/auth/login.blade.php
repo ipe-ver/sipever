80cbcec5065feda0205556f253b4a0ad5ae80c12
yessica luna <luna.rico.karla.yessica@gmail.com>
2019-10-15 10:30:15 -0500

    Modifico la vista de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">Correo electrónico o nombre de usuario</label>
+                            <label for="email" class="col-md-4 control-label">Nombre de usuario</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}">

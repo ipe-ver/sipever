8aa2840f1b1c620e9a342ec078e7d6eb5c7ebb7f
yessica luna <luna.rico.karla.yessica@gmail.com>
2019-10-18 07:44:19 -0500

    Modifico el seed de la tabla de empleados

--- database/seeds/CatEmpleadosTableSeeder.php
+++ database/seeds/CatEmpleadosTableSeeder.php
@@ -15,22 +15,31 @@ class CatEmpleadosTableSeeder extends Seeder
     {
         
         $empleados = array(
-			array( // row #0
+			array( // row #1
                 'id' => 1,
                 'no_personal' => 2867,
                 'fecha_ingreso' => '2018/12/07',
                 'apellido_paterno' => 'CASTILLO',
-                'apellido_materno' => 'BARVO',
+                'apellido_materno' => 'BRAVO',
                 'nombre' => 'SERGIO ARGENIS',
 				'estatus' => 1,
             ),
-            array( // row #0
+            array( // row #2
                 'id' => 2,
                 'no_personal' => 2851,
                 'fecha_ingreso' => '2018/12/01',
                 'apellido_paterno' => 'GUTIERREZ',
                 'apellido_materno' => 'RENDON',
                 'nombre' => 'ERICK SAMUEL',
+				'estatus' => 1,
+            ),
+            array( // row #3
+                'id' => 3,
+                'no_personal' => 2891,
+                'fecha_ingreso' => '2019/06/03',
+                'apellido_paterno' => 'LUNA',
+                'apellido_materno' => 'RICO',
+                'nombre' => 'KARLA YESSICA',
 				'estatus' => 1,
             ),
 			

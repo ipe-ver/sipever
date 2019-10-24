2a8f6350ac60fb2df31a7b585622669ac8042511
JosafatMurillo <josagears@hotmail.com>
2019-10-24 10:53:18 -0500

    Se agregan sp's y dos tablas

---
+++ database/migrations/2019_10_01_163252_create_table_d_pedido_compra.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableDPedidoCompra extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('d_pedido_compra', function (Blueprint $table) {
+            $table->increments('id_pedido_compra_d');
+
+            $table->integer('id_pedido_compra')->unsigned();
+            $table->foreign('id_pedido_compra')->references('id_pedido_consumo')->on('c_pedido_consumo');
+
+            $table->integer('id_articulo')->unsigned();
+            $table->foreign('id_articulo')->references('id')->on('cat_articulos_compra');
+
+            $table->integer('cantidad');
+            $table->integer('no_folio');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('d_pedido_compra');
+    }
+}
